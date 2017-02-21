b25a97aac381803e2893f6dfc7ff6f128f28b68d
Sergey <sereonyka@yandex.ru>
2017-02-21 15:52:12 +0300

    Update Module.ts
    ref typo

--- src/Module.ts
+++ src/Module.ts
@@ -1,7 +1,7 @@
 import * as angular from "angular";
 import MenuComponent from "./components/Menu/MenuComponent";
 import MenuItemComponent from "./components/MenuItem/MenuItemComponent";
-import FakedataService from "./servises/FakeData/FakeDataServise";
+import FakedataService from "./services/FakeData/FakeDataService";
 import RootComponent from "./components/Root/RootComponent";
 
 let app = angular.module("app", []);
